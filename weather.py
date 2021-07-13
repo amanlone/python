28df8d019a7b710a2db695813bb279561771e578
amanlone <86300281+amanlone@users.noreply.github.com>
2021-07-13 19:51:14 +0800

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,21 @@
+
+import pyowm
+def report():
+        owm = pyowm.OWM("1c3938cd971b64b8e3d796d59deb4cbb")
+        mgr = owm.weather_manager()
+        country = input("Enter a country or a city\n")
+        observation = mgr.weather_at_place(country)
+        weth = observation.weather
+        temp = weth.temperature("celsius")
+        status = weth.detailed_status
+
+        print(int(temp["temp"]))
+        print(status)
+report()
+while True:
+    leave = input("Would you like to find weather forecast for another country? (y/n)\n")
+    if leave == "n":
+        report()
+
+    else:
+        exit()
